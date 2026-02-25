38a6c53008d273d4641dfd1bcc7e2955dfca0c71
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-02-25 16:16:49 +0800

    chore: Phase 1 - update config.py

--- pipeline/utils/config.py
+++ pipeline/utils/config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_CONFIG = {
-    "host": os.environ.get("DB_HOST", "localhost"),
-    "port": os.environ.get("DB_PORT", "5432"),
-    "dbname": os.environ.get("DB_NAME", "sg_cost_living"),
-    "user": os.environ.get("DB_USER", "postgres"),
-    "password": os.environ.get("DB_PASSWORD", "")
+    "host": os.environ.get("DB_HOST"),
+    "port": os.environ.get("DB_PORT"),
+    "dbname": os.environ.get("DB_NAME"),
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASSWORD")
 }
 
 DATAGOV_BASE_URL = os.environ.get(
